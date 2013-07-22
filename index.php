f65acabf32e9b3e95ea1bb8611d0d1993f7b6df1
sbuggay <sbuggay@gmail.com>
2013-07-22 18:50:25 +0000

    added database test

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
 <html>
 
+<head>
 
 
+</head>
+
+<body>
 
 
+<?php
+$dbh = pg_connect("host=localhost dbname=test user=postgres");
+if (!$dbh) {
+	die("Error in connection: " . pg_last_error());
+}
 
+$sql = "SELECT * FROM People";
+$result = pg_query($dbh, $sql);
+
+
+if (!$result) {
+	die("Error in SQL query: " . pg_last_error());
+}
+
+while ($row = pg_fetch_array($result)) {
+	echo "id: " . $row[0] . "<br />";
+	echo "name: " . $row[1] . "<p />";
+}
+
+pg_free_result($result);
+pg_close($dbh);
+?>
+
+
+</body>
 </html>
