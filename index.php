298d26d7bfe2eae2c3de26e4f841fa2fc63fc535
sbuggay <sbuggay@gmail.com>
2013-07-23 22:25:02 -0500

    Fixed a lot of stuff

--- index.php
+++ index.php
@@ -1,74 +1,216 @@
-<html>
+<!DOCTYPE html>   
+<html lang="en">   
 
 <head>
-
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+  <style type="text/css">
+    /*.form-container {
+        width: 300px;
+        clear: both;
+    }
+    .form-container input {
+        width: 100%;
+        clear: both;
+    }
+    */
+  </style>
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
 </head>
 
 
 
 <div class="container">
 
-<body>
-<div class="navbar navbar-inverse">
-  <div class="navbar-inner">
-    <a class="brand" href="#">Bay View Community Hospital</a>
-    <ul class="nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Patient Bill</a></li>
-      <li><a href="#">Room Utilization Report</a></li>
-      <li><a href="#">Patient Report</a></li>
-      <li><a href="#">Physician Report</a></li>
-    </ul>
+  <body>
+    <div class="navbar navbar-inverse">
+      <div class="navbar-inner">
+        <a class="brand" href="#">Bay View Community Hospital</a>
+        <ul class="nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Patient Bill</a></li>
+          <li><a href="#">Room Utilization Report</a></li>
+          <li><a href="#">Patient Report</a></li>
+          <li><a href="#">Physician Report</a></li>
+        </ul>
+      </div>
+    </div>
+
+
+    <?php
+
+// insert form into database
+    if ($_POST['submit']) {
+    // attempt a connection
+      $dbh = pg_connect("host=localhost dbname=group5db user=postgres password=hendrix");
+      if (!$dbh) {
+        die("Error in connection: " . pg_last_error());
+      }
+    // escape strings in input data
+      $id = pg_escape_string($_POST['cid']);
+      $name = pg_escape_string($_POST['cname']);
+      $street = pg_escape_string($_POST['cstreet']);
+      $city = pg_escape_string($_POST['ccity']);
+      $state = pg_escape_string($_POST['cstate']);
+      $zip = pg_escape_string($_POST['czip']);
+      $adate = pg_escape_string($_POST['cadate']);
+      $ddate = pg_escape_string($_POST['cddate']);
+      $doctorid = pg_escape_string($_POST['cdid']);
+      $insurance = pg_escape_string($_POST['cinsurancename']);
+      $policy = pg_escape_string($_POST['cpolicy']);
+      $roomid = pg_escape_string($_POST['croomid']);
+      $bedlabel = pg_escape_string($_POST['cbedlabel']);
+
+    // execute query
+      $sql = "INSERT INTO Patient VALUES('$id', '$name', '$street', '$city', '$state', '$zip', '$adate', '$ddate', '$doctorid', '$insurance', '$policy', '$roomid', '$bedlabel')";
+      $result = pg_query($dbh, $sql);
+
+      if (!$result) {
+        die("Error in SQL query: " . pg_last_error());
+      }
+
+      echo "Data successfully inserted!";
+
+      pg_free_result($result);
+      pg_close($dbh);
+    }
+    ?>
+    
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" class="well" method="post">
+      <fieldset>
+        <legend>New Patient</legend>
+        <label>Patient ID:</label> <input type="number" name="cid"> 
+        <label>name:</label> <input type="text" name="cname"> 
+        <label>Street:</label> <input type="text" name="cstreet"> 
+        <label>City:</label> <input type="text" name="ccity"> 
+        <label>State:</label> <input type="text" name="cstate"> 
+        <label>ZIP:</label> <input type="text" name="czip"> 
+        <label>Admitted Date:</label> <input type="date" name="cadate"> 
+        <label>Discharged Date:</label> <input type="date" name="cddate"> 
+        <label>DoctorID:</label> <input type="number" name="cdid"> 
+        <label>Insurance Name:</label> <input type="text" name="cinsurancename"> 
+        <label>Policy Number:</label> <input type="text" name="cpolicy"> 
+        <label>Room ID:</label> <input type="number" name="croomid"> 
+        <label>Bed Label:</label> <input type="text" name="cbedlabel"> 
+        <br>
+        <input class="btn btn-primary" type="submit" name="submit">
+      </fieldset>
+    </form>
+
+    <h3>Patients</h3>
+    <?php
+    $dbh = pg_connect("host=localhost dbname=group5db user=postgres password=hendrix");
+    if (!$dbh) {
+      echo("Error in connection: " . pg_last_error());
+    }
+
+    $sql = "SELECT * FROM Patient";
+    $result = pg_query($dbh, $sql);
+    if (!$result) {
+      echo("Error in SQL query: " . pg_last_error());
+    }
+
+    while ($row = pg_fetch_array($result)) {
+      echo "<pre>";
+      echo "ID: " . $row[0] . "<br />";
+      echo "Name: " . $row[1] . "<br />";
+      echo "Street: " . $row[2] . "<br />";
+      echo "City: " . $row[3] . "<br />";
+      echo "State: " . $row[4] . "<br />";
+      echo "ZIP: " . $row[5] . "<br />";
+      echo "Admitted Date: " . $row[6] . "<br />";
+      echo "Discharged Date: " . $row[7] . "<br />";
+      echo "Doctor ID: " . $row[8] . "<br />";
+      echo "Policy Number: " . $row[9] . "<br />";
+      echo "Room ID: " . $row[10] . "<br />";
+      echo "Bed Label: " . $row[11] . "<br />";
+      echo "</pre>";
+    }
+
+    pg_free_result($result);
+    pg_close($dbh);
+    ?> 
+
+    <h3>Insurances</h3>
+    <?php
+    $dbh = pg_connect("host=localhost dbname=group5db user=postgres password=hendrix");
+    if (!$dbh) {
+      echo("Error in connection: " . pg_last_error());
+    }
+
+    $sql = "SELECT * FROM Insurance";
+    $result = pg_query($dbh, $sql);
+    if (!$result) {
+      echo("Error in SQL query: " . pg_last_error());
+    }
+
+    while ($row = pg_fetch_array($result)) {
+      echo "<pre>";
+      echo "Name: " . $row[0] . "<br />";
+      echo "Phone: " . $row[1] . "<br />";
+      echo "</pre>";
+    }
+
+    pg_free_result($result);
+    pg_close($dbh);
+    ?>
+
+    <h3>Treatment Types</h3>
+    <?php
+    $dbh = pg_connect("host=localhost dbname=group5db user=postgres password=hendrix");
+    if (!$dbh) {
+      echo("Error in connection: " . pg_last_error());
+    }
+
+    $sql = "SELECT * FROM TreatmentType";
+    $result = pg_query($dbh, $sql);
+    if (!$result) {
+      echo("Error in SQL query: " . pg_last_error());
+    }
+
+    while ($row = pg_fetch_array($result)) {
+      echo "<pre>";
+      echo "Treatment: " . $row[0] . "<br />";
+      echo "Cost: " . $row[1] . "<br />";
+      echo "Ordered: " . $row[2] . "<br />";
+      echo "</pre>";
+    }
+
+    pg_free_result($result);
+    pg_close($dbh);
+    ?>
+
+    <h3>Employee Costs</h3>
+    <?php
+    $dbh = pg_connect("host=localhost dbname=group5db user=postgres password=hendrix");
+    if (!$dbh) {
+      echo("Error in connection: " . pg_last_error());
+    }
+
+    $sql = "SELECT * FROM EmployeeCost";
+    $result = pg_query($dbh, $sql);
+    if (!$result) {
+      echo("Error in SQL query: " . pg_last_error());
+    }
+    
+    while ($row = pg_fetch_array($result)) {
+      echo "<pre>";
+      echo "Type: " . $row[0] . "<br />";
+      echo "Cost: " . $row[1] . "<br />";
+      echo "</pre>";
+    }
+    
+    pg_free_result($result);
+    pg_close($dbh);
+    ?>
+
+
+
   </div>
-</div>
 
-<h2>Database Test</h2>
-<pre>
-<?php
-
-$dbh = pg_connect("host=localhost dbname=test user=postgres password=hendrix");
-if (!$dbh) {
-	echo("Error in connection: " . pg_last_error());
-}
-
-$sql = "SELECT * FROM People";
-$result = pg_query($dbh, $sql);
-
-
-if (!$result) {
-	echo("Error in SQL query: " . pg_last_error());
-}
-
-while ($row = pg_fetch_array($result)) {
-	echo "id: " . $row[0] . "<br />";
-	echo "name: " . $row[1] . "<p />";
-}
-
-pg_free_result($result);
-pg_close($dbh);
-?>
-</pre>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-
-
-     id: <br> <input type="text" name="ccode" size="2">  
-
-
-     <p>
-
-
-     name: <br> <input type="text" name="cname">       
-
-
-     <p>
-
-
-     <input class="btn btn-primary" type="submit" name="submit">
-
-</form> 
-
-</div>
-
+  <script type="text/javascript">
+    jQuery(document).ready(function ($) {
+      $(".tabs").tabs();
+    });
+  </script>    
+  <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ table.sql
@@ -0,0 +1,155 @@
+DROP TABLE Room CASCADE;
+DROP TABLE Bed CASCADE;
+DROP TABLE Insurance CASCADE;
+DROP TABLE EmployeeCost CASCADE;
+DROP TABLE Patient CASCADE;
+DROP TABLE TreatmentType CASCADE;
+DROP TABLE Treatment CASCADE;
+DROP TABLE Doctor CASCADE;
+DROP TABLE TreatmentInvolvement CASCADE;
+
+CREATE TABLE Room(
+        roomID integer NOT NULL,
+        roomPrivate boolean,
+        numBeds smallint,
+        Cost smallint,
+	PRIMARY KEY (roomID)
+        );
+
+CREATE TABLE Doctor(
+	DoctorID smallint NOT NULL,
+	name varchar(30),
+	phone varchar(15),
+	PRIMARY KEY (DoctorID)
+	);
+
+CREATE TABLE Bed(
+        roomID integer NOT NULL,
+        bedLabel varchar(1) NOT NULL,
+        PRIMARY KEY (roomID,bedLabel),
+        FOREIGN KEY (roomID) REFERENCES Room(roomID)
+        );
+
+CREATE TABLE Insurance(
+	InsuranceName varchar(20) NOT NULL,
+	phone varchar(20),
+	PRIMARY KEY (insuranceName)
+	);
+
+CREATE TABLE EmployeeCost(
+	employeeType varchar(20),
+	cost smallint,
+	PRIMARY KEY (employeeType)
+	);
+
+CREATE TABLE Patient(
+	patientID smallint NOT NULL,
+	name varchar(30),
+	addrStreet varchar(50),
+	addrCity varchar(25),
+	addrState varchar(15),
+	addrZIP integer,
+	admitted date,
+	discharged date,
+	doctorID smallint,
+	insuranceName varchar(20),
+	policynum varchar(20),
+	roomID integer,
+	bedLabel varchar(1),
+	PRIMARY KEY (patientID),
+	FOREIGN KEY (roomID, bedLabel) REFERENCES Bed (RoomID, bedLabel),
+	FOREIGN KEY (insuranceName) REFERENCES Insurance (insuranceName),
+	FOREIGN KEY (doctorID) REFERENCES Doctor (DoctorID)
+	);
+
+CREATE TABLE TreatmentType(
+	TreatmentType varchar(20) NOT NULL,
+	cost smallint,
+	ordered boolean,
+	PRIMARY KEY (TreatmentType)
+	);
+
+CREATE TABLE Treatment(
+	TreatmentID smallint NOT NULL,
+	TreatmentType varchar(20) NOT NULL,
+	doctorID smallint,
+	patientID smallint NOT NULL,
+	duration smallint,
+	PRIMARY KEY (TreatmentID),
+	FOREIGN KEY (patientID) REFERENCES Patient (PatientID),
+	FOREIGN KEY (TreatmentType) REFERENCES TreatmentType (TreatmentType),
+	FOREIGN KEY (doctorID) REFERENCES Doctor(DoctorID) 
+	);
+
+CREATE TABLE TreatmentInvolvement(
+	TreatmentID smallint NOT NULL,
+	EmployeeType varchar(20) NOT NULL,
+	Count smallint,
+	PRIMARY KEY (TreatmentID, EmployeeType),
+	FOREIGN KEY (EmployeeType) REFERENCES EmployeeCost(EmployeeType),
+	FOREIGN KEY (TreatmentID) REFERENCES Treatment(TreatmentID)
+);
+
+INSERT INTO TreatmentType VALUES
+	('Surgery1',10000,true),
+	('Surgery2',7000,true),
+	('Outpatient1',1500,true),
+	('Outpatient2',900,true),
+	('Outpatient3',500,true),
+	('PhysTherapy',300,true),
+	('XRay',200,true),
+	('BloodTest',75,true),
+	('RespTherapy',200,true),
+	('RoomVisit',50,false),
+	('AttendantCare',50,false),
+	('DrConsult',100,true),
+	('PrivateRoom',1000,false),
+	('SemiPrivateRoom',500,false)
+;	
+
+INSERT INTO EmployeeCost VALUES
+	('Doctor', 300),
+	('Nurse', 150),
+	('Technician', 75),
+	('Staff', 25)
+;
+
+INSERT INTO Room VALUES
+	(101, false, 2, 100),
+	(102, false, 2, 100),
+	(103, false, 2, 100),
+	(104, false, 2, 100),
+	(201, false, 2, 100),
+	(202, false, 2, 100),
+	(203, false, 2, 100),
+	(204, false, 2, 100)
+;
+
+INSERT INTO Bed VALUES
+	(101, 'A'),
+	(101, 'B'),
+	(102, 'A'),
+	(102, 'B'),
+	(103, 'A'),
+	(103, 'B'),
+	(104, 'A'),
+	(104, 'B'),
+	(201, 'A'),
+	(201, 'B'),
+	(202, 'A'),
+	(202, 'B'),
+	(203, 'A'),
+	(203, 'B'),
+	(204, 'A'),
+	(204, 'B')
+;
+
+INSERT INTO Insurance VALUES
+	('Medicare', '000 000 0000'),
+	('Medicare', '000 000 0000')
+;
+
+INSERT INTO Doctor VALUES
+	(1, 'Doctor 1', '000 000 0000'),
+	(2, 'Doctor 2', '000 000 0000')
+;
